287c5d482a1f47730b5045cf48f7fc59e3eb9c17
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-06-03 16:15:51 +0200

    fix: price

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -21,9 +21,10 @@ class ProductsTableSeeder extends Seeder
             $newProduct = new Product();
             $newProduct->name = $faker->sentence(2);
             $newProduct->slug = Str::slug($newProduct->name, '-');
-            $newProduct->price = $faker->randomFloat(2, 10, 9000);
+            $newProduct->price = $faker->randomFloat(2, 10, 500);
             $newProduct->description = $faker->text(500);
             $newProduct->img = 'https://picsum.photos/200/300';
+            $newProduct->save();
         }
     }
 }
