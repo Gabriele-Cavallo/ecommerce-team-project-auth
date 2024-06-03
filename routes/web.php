e733173d3dd6d5b4a1f1faaca0f6ef60da1d5e2f
Damiano <ghgg585@gmail.com>
2024-06-03 16:40:07 +0200

    feat add crud resource

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+->name('admin.')
+->prefix('admin')
+->group(function(){
+    //rotte amministrazione
+    Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/products', ProductsController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
