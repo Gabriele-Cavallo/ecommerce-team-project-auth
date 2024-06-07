662d80758ef798b152126b8554b05486873fd87d
DamianoSpampinato <ghgg585@gmail.com>
2024-06-07 16:38:00 +0200

    feat add index orders crud

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,4 +1,44 @@
 @extends('layouts.admin')
 @section('content')
-    sono la index
+    @if (empty($orders))
+        <div>non ci sono ordini da visualizzare</div>
+        
+    @else
+    <table class="table">
+        <thead class="thead-dark">
+
+            <tr>
+                <th>Id</th>
+                <th>Date</th>
+                <th>Username</th>
+                <th>Quantity</th>
+                <th>Address</th>
+                <th>Total price</th>
+            </tr>
+        </thead>
+        <tbody class="table-striped">
+            
+            @foreach ($orders as $order)
+            <tr>
+                <td>
+                    $order->id
+                </td>
+                <td>
+                    $order->date
+                </td>
+                <td>$order->user_id</td>
+                <td>
+                    $order->amount
+                </td>
+                <td>
+                    $order->address
+                </td>
+                <td>
+                    $order->price
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @endif
 @endsection
